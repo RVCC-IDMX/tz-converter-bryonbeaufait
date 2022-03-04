0c7da31b5c2b549510b6897fa9e5c72f75f28066
Bryon Beaufait <beaufaitb@gmail.com>
2022-03-04 16:16:57 -0500

    add timezone converter

---
+++ index.js
@@ -0,0 +1,11 @@
+const moment = require('moment-timezone');
+
+moment.tz.setDefault('America/New_York');
+
+const targetTimezone = 'Europe/Paris';
+
+console.log(
+  `The time at the ${targetTimezone} timezone is ${moment()
+    .tz(targetTimezone)
+    .format()}`
+);
