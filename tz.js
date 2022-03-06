016df09b9b99bdc5158da5ef510cbbdf6412354e
Bryon Beaufait <beaufaitb@gmail.com>
2022-03-06 10:18:47 -0500

    add yargs

--- tz.js
+++ tz.js
@@ -1,18 +1,32 @@
+const yargs = require('yargs');
 const moment = require('moment-timezone');
+const { format } = require('prettier');
 
 moment.tz.setDefault('America/New_York');
+const timezone = moment().tz('America/New_York').format();
+const timezone_formatted = moment()
+  .tz('America/New_York')
+  .format('dddd, MMMM Do YYYY, h:mm:ss a');
 
-let targetTimezone;
+let command = yargs.argv._[0];
+let params = yargs.argv;
 
-if (process.argv.length != 3) {
-  console.log('Usage: node <script-file><timezone>');
-  process.exit(1);
+if (command === 'timezone') {
+  if (params.time && params.format) {
+    console.log(`The time here is: ${timezone_formatted}`);
+    console.log(
+      `The time at the ${params.time} timezone is: ${moment()
+        .tz(params.time)
+        .format('dddd, MMMM Do YYYY, h:mm:ss a')}`
+    );
+  } else if (params.time) {
+    console.log(`The time here is: ${timezone}`);
+    console.log(
+      `The time at the ${params.time} timezone is ${moment()
+        .tz(params.time)
+        .format()}`
+    );
+  }
 } else {
-  targetTimezone = process.argv[2];
+  console.log('Usage: node tz <timezone> [--time=target_timezone] [--format]');
 }
-
-console.log(
-  `The time at the ${targetTimezone} timezone is ${moment()
-    .tz(targetTimezone)
-    .format()}`
-);
